e0b2eafe1461aadf8ebc7cf126bebdec53cb2cce
Jasdeep Gill <jazgill@fb.com>
2020-07-08 11:06:31 -0700

    completed lab

---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,114 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
+#import "LocationsViewController.h"
+
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate,LocationsViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIImageView *mapImageView;
+
+
+@end
+
+@implementation PhotoMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.mapView.delegate = self;
+    // Do any additional setup after loading the view.
+    //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)didTapButton:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.mapImageView.image = originalImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:^{
+        [self performSegueWithIdentifier:@"tagSegue" sender:nil];
+    }];
+    
+}
+
+- (void)locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude{
+    [self.navigationController popToViewController:self animated:(YES)];
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = [NSString stringWithFormat:@"%f", latitude.floatValue];
+    [self.mapView addAnnotation:annotation];
+    [self.mapView viewForAnnotation:annotation];
+    
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    imageView.image = [UIImage imageNamed:@"camera-l"];
+
+    return annotationView;
+}
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+//    UINavigationController *locationViewController = [segue destinationViewController];
+//    LocationsViewController *locationsController =  (LocationsViewController*)locationViewController.topViewController;
+//
+//    locationsController.delegate = self;
+    
+    LocationsViewController *locationsViewController = [segue destinationViewController];
+    locationsViewController.delegate = self;
+    
+}
+
+
+@end
